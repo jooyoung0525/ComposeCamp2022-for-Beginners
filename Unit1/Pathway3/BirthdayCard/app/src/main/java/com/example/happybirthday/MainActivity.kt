49dd44a690a7673c12be813aa836c39e8e0890ea
jooyoung <jooyoung0525@github.com>
2022-11-30 11:09:53 +0900

    Image, Text, modifier
    앱의 사용자 인터페이스 : 화면에 표시되는 텍스트, 이미지, 버튼, etc...
    Jetpack Compose : 안드로이드 UI를 빌드하기위해 사용하는 최신 도구 키트
    Compose의 세가지 기준 레이아웃 요소 : Row, Column, Box
    
    <Composable>
    @Composable 주석을 통해 함수가 ui로 변환하는것을 컴파일러에게 알림
    @Composable 주석을 사용한 함수 이름은 파스칼 표기법으로 명시. -> 명사, 형용사+명사, 모든 단어 대문자 표기
    
    getString이랑 stringResource랑 무슨차이?

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -15,6 +15,7 @@
  */
 package com.example.happybirthday
 
+import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import android.text.Layout
 import androidx.activity.ComponentActivity
@@ -27,6 +28,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,7 +40,16 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            HappyBirthdayTheme() {
+                Surface(color = Color.LightGray) {
+                    BirthdayGreetingWithText(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.from_text)
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -46,15 +57,43 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
     // Create a column so that texts don't overlap
-    Column { }
+    Column {
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally).padding(top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
+        )
+    }
 }
 
 // 5. Box 레이아웃 추
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
 
 // 4. 이미지 컴포저블 추가
-@Preview(showBackground = false)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-private fun BirthdayCardPreview() { }
+private fun BirthdayCardPreview() {
+    HappyBirthdayTheme() {
+        // BirthdayGreetingWithText(message = "Happy Birthday!", from = "From. Jooyoung")
+        BirthdayGreetingWithImage(message = "Happy Birthday!", from = "From. Jooyoung")
+    }
+}
 
